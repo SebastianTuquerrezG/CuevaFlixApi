4493ca3d30bfa36f1d19511f08b989ec263d7f3a
Sebastian <jtuquerrez@unicauca.edu.co>
2023-02-09 19:41:32 -0500

    Project Finished, only with bug protocols for a future

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,11 +3,16 @@ import api from './api/axiosConfig';
 import {useState,useEffect} from 'react';
 import Layout from './components/layout';
 import {Routes,Route} from 'react-router-dom';
-import Home from './components/home/Home'
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Revies';
 
 function App() {
 
   const [movies,setMovies]=useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
   
   const getMovies = async()=>{
     try{
@@ -19,15 +24,33 @@ function App() {
     }
   }
 
+  const getMovieData = async (movieId) => {
+      
+    try 
+    {
+      const response = await api.get(`/api/cuevaflix/${movieId}`);
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviews);
+    } 
+    catch (error) 
+    {
+      console.error(error);
+    }
+  }
+
   useEffect(()=>{
     getMovies();
   },[])
 
   return (
     <div className="App">  
+      <Header/>
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies={movies}/>}/>
+          <Route path="/trailer/:ytTrailerId" element={<Trailer/>}/>
+          <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
         </Route>
       </Routes>
     </div>
